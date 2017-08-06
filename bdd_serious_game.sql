6d2dd9171825ec58851fd6b991ac69912b8b63f6
yguivarch24 <yoannkone@gmail.com>
2017-08-06 21:46:17 +1100

    bdd serious games remplie 1 et améliorer
    Un peu remplie

--- bdd_serious_game.sql
+++ bdd_serious_game.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
--- Client :  127.0.0.1
--- Généré le :  Mer 02 Août 2017 à 06:37
--- Version du serveur :  10.1.19-MariaDB
--- Version de PHP :  5.6.28
+-- Hôte : 127.0.0.1
+-- Généré le :  Dim 06 août 2017 à 12:11
+-- Version du serveur :  10.1.25-MariaDB
+-- Version de PHP :  7.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -30,8 +32,9 @@ CREATE TABLE `avatar` (
   `id_avt` int(10) NOT NULL,
   `nom_img` varchar(50) COLLATE utf8_bin NOT NULL,
   `taille_img` varchar(25) COLLATE utf8_bin NOT NULL,
-  `type_img` int(25) NOT NULL,
-  `blob_img` blob NOT NULL
+  `type_img` varchar(25) COLLATE utf8_bin NOT NULL,
+  `blob_img` blob NOT NULL,
+  `id_util` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -44,6 +47,13 @@ CREATE TABLE `a_repondue` (
   `id_util` int(10) NOT NULL,
   `id_q` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `a_repondue`
+--
+
+INSERT INTO `a_repondue` (`id_util`, `id_q`) VALUES
+(1, 1);
 
 -- --------------------------------------------------------
 
@@ -93,6 +103,13 @@ CREATE TABLE `filiere` (
   `nom_fil` varchar(50) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Déchargement des données de la table `filiere`
+--
+
+INSERT INTO `filiere` (`id_fil`, `nom_fil`) VALUES
+(1, 'informatique');
+
 -- --------------------------------------------------------
 
 --
@@ -115,6 +132,14 @@ CREATE TABLE `matiere` (
   `titre_mat` varchar(20) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Déchargement des données de la table `matiere`
+--
+
+INSERT INTO `matiere` (`id_mat`, `titre_mat`) VALUES
+(1, 'Arts et Litterature'),
+(2, 'Sciences et Nature');
+
 -- --------------------------------------------------------
 
 --
@@ -128,6 +153,15 @@ CREATE TABLE `question` (
   `id_mat` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Déchargement des données de la table `question`
+--
+
+INSERT INTO `question` (`id_q`, `description`, `valeur`, `id_mat`) VALUES
+(1, 'Qui a peint \"Le chat angora\" ?', 5, 1),
+(2, 'Qui a peint \"Le plagiat\" ?', 5, 1),
+(3, 'Qui a peint \"Le bal à Bougival\" ?', 5, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -136,9 +170,21 @@ CREATE TABLE `question` (
 
 CREATE TABLE `reponse` (
   `id_r` int(10) NOT NULL,
-  `description` text COLLATE utf8_bin NOT NULL,
-  `type_rep` varchar(20) COLLATE utf8_bin NOT NULL
+  `description` text COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `reponse`
+--
+
+INSERT INTO `reponse` (`id_r`, `description`) VALUES
+(1, 'Honoré Fragonard'),
+(2, 'Gustave Courbet'),
+(3, 'Henri Matisse'),
+(4, 'Pablo Picasso'),
+(5, 'Paul Cézanne'),
+(6, 'René Magritte'),
+(7, 'Auguste Renoir');
 
 -- --------------------------------------------------------
 
@@ -151,6 +197,30 @@ CREATE TABLE `reponse_possible` (
   `id_r` int(10) NOT NULL,
   `type_rep` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `reponse_possible`
+--
+
+INSERT INTO `reponse_possible` (`id_q`, `id_r`, `type_rep`) VALUES
+(1, 1, 1),
+(1, 2, 0),
+(1, 3, 0),
+(1, 4, 0),
+(1, 5, 0),
+(1, 6, 0),
+(2, 1, 0),
+(2, 2, 0),
+(2, 3, 0),
+(2, 4, 0),
+(2, 5, 0),
+(2, 6, 1),
+(3, 1, 0),
+(3, 3, 0),
+(3, 4, 0),
+(3, 5, 0),
+(3, 6, 0),
+(3, 7, 1);
 
 -- --------------------------------------------------------
 
@@ -165,21 +235,29 @@ CREATE TABLE `utilisateur` (
   `email` varchar(50) COLLATE utf8_bin NOT NULL,
   `nom_util` varchar(20) COLLATE utf8_bin NOT NULL,
   `mot_de_passe` varchar(20) COLLATE utf8_bin NOT NULL,
+  `niveau` int(3) NOT NULL DEFAULT '0',
   `titre` varchar(20) COLLATE utf8_bin NOT NULL DEFAULT 'utilisateur',
   `admin` tinyint(1) NOT NULL DEFAULT '0',
-  `id_avt` int(10) DEFAULT NULL,
   `id_fil` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Index pour les tables exportées
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id_util`, `nom`, `prenom`, `email`, `nom_util`, `mot_de_passe`, `niveau`, `titre`, `admin`, `id_fil`) VALUES
+(1, 'Guivarch', 'Yoann', 'yoann.guivarch@etudiant.univ-nc.nc', 'azerty', 'azerty', 0, 'admin', 1, 1);
+
+--
+-- Index pour les tables déchargées
 --
 
 --
 -- Index pour la table `avatar`
 --
 ALTER TABLE `avatar`
-  ADD PRIMARY KEY (`id_avt`);
+  ADD PRIMARY KEY (`id_avt`),
+  ADD KEY `id_util` (`id_util`);
 
 --
 -- Index pour la table `a_repondue`
@@ -219,7 +297,8 @@ ALTER TABLE `filiere`
 -- Index pour la table `liste_ami`
 --
 ALTER TABLE `liste_ami`
-  ADD PRIMARY KEY (`id_ami1`,`id_ami2`);
+  ADD PRIMARY KEY (`id_ami1`,`id_ami2`),
+  ADD KEY `id_ami2` (`id_ami2`);
 
 --
 -- Index pour la table `matiere`
@@ -252,11 +331,10 @@ ALTER TABLE `reponse_possible`
 --
 ALTER TABLE `utilisateur`
   ADD PRIMARY KEY (`id_util`),
-  ADD KEY `id_avt` (`id_avt`),
   ADD KEY `id_fil` (`id_fil`);
 
 --
--- AUTO_INCREMENT pour les tables exportées
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
@@ -268,30 +346,36 @@ ALTER TABLE `avatar`
 -- AUTO_INCREMENT pour la table `filiere`
 --
 ALTER TABLE `filiere`
-  MODIFY `id_fil` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_fil` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `matiere`
 --
 ALTER TABLE `matiere`
-  MODIFY `id_mat` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_mat` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `question`
 --
 ALTER TABLE `question`
-  MODIFY `id_q` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_q` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT pour la table `reponse`
 --
 ALTER TABLE `reponse`
-  MODIFY `id_r` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_r` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `id_util` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_util` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
--- Contraintes pour les tables exportées
+-- Contraintes pour les tables déchargées
 --
+
+--
+-- Contraintes pour la table `avatar`
+--
+ALTER TABLE `avatar`
+  ADD CONSTRAINT `avatar_ibfk_1` FOREIGN KEY (`id_util`) REFERENCES `utilisateur` (`id_util`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `a_repondue`
@@ -322,6 +406,13 @@ ALTER TABLE `demande_defi`
   ADD CONSTRAINT `demande_defi_ibfk_2` FOREIGN KEY (`id_rec`) REFERENCES `utilisateur` (`id_util`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Contraintes pour la table `liste_ami`
+--
+ALTER TABLE `liste_ami`
+  ADD CONSTRAINT `liste_ami_ibfk_1` FOREIGN KEY (`id_ami1`) REFERENCES `utilisateur` (`id_util`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `liste_ami_ibfk_2` FOREIGN KEY (`id_ami2`) REFERENCES `utilisateur` (`id_util`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Contraintes pour la table `question`
 --
 ALTER TABLE `question`
@@ -338,8 +429,8 @@ ALTER TABLE `reponse_possible`
 -- Contraintes pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  ADD CONSTRAINT `utilisateur_ibfk_1` FOREIGN KEY (`id_avt`) REFERENCES `avatar` (`id_avt`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `utilisateur_ibfk_2` FOREIGN KEY (`id_fil`) REFERENCES `filiere` (`id_fil`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
